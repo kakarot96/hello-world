dc34828c15d40c59cfcd61177e3b6c5c272b6c5c
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-12 16:04:44 +0530

    EventBind method 1

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
 import Message from './Components/Message'
 import Counter from './Components/Counter'
+import EventBind from './Components/EventBind';
 
 function App() {
   return (
     <div className="App">
+      <EventBind/>
       {/* <Message/> */}
-      <Counter/>
+      {/* <Counter/> */}
       {/* <Greet/>
       <Welcome/> */}
     </div>
---
+++ src/Components/EventBind.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+ class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            message : "Hello"
+        }
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+    clickHandler(){
+        this.setState({
+            message: "GoodBye!"
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick = {this.clickHandler}>Click</button>
+            </div>
+        )
+    }
+}
+
+export default EventBind
