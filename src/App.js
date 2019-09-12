be7996509e8b77ccdc44e8c7200c12c58dfd8625
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-12 13:21:56 +0530

    Using state

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
+import Message from './Components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet/>
-      <Welcome/>
+      <Message/>
+      {/* <Greet/>
+      <Welcome/> */}
     </div>
   );
 }
---
+++ src/Components/Message.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+
+class Message extends Component{
+    constructor(){
+        super();
+        this.state = {
+            message : "Welcome"
+        }
+    }
+     changeState(){
+        this.setState({
+            message : "Thanks for Subscribing"
+        });
+    }
+    render(){
+        return (
+            <div>
+                <h1>{this.state.message}</h1>
+                <button onClick={()=> this.changeState()}>Subscribe</button>
+            </div>
+        )
+    }
+}
+
+export default Message
