60cf7d7eb8626f957ccfc81c81b1071e8bbc7f39
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-12 17:21:22 +0530

    List Rendering with separate component for Person

--- src/Components/NameList.js
+++ src/Components/NameList.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Person from "./Person";
 
 function NameList() {
-  const names = ["Diana", "Clarke", "Bruce"];
-  const nameList = names.map(name => <h2>{name}</h2>);
-  return <div>{nameList}</div>;
+  const persons = [
+    { id: 1, name: "Bruce", age: 50, skills: "React" },
+    { id: 2, name: "Diana", age: 150, skills: "Angular" },
+    { id: 3, name: "Clarke", age: 45, skills: "Vue" }
+  ];
+  const personsList = persons.map(person => <Person person={person}></Person> );
+  return <div>{personsList}</div>;
 }
 
 export default NameList;
---
+++ src/Components/Person.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Person({person}) {
+    // console.log(props)
+    return (
+        <div>
+            <h2>
+                
+                I am {person.name }. I am {person.age} years old and I know {person.skills}
+            </h2>
+        </div>
+    );
+}
+
+export default Person;
