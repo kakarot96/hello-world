7f2770b68b9249acbb4e7c3e77171387cac23cdd
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-12 15:28:35 +0530

    setState with callback

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 import Greet from './Components/Greet'
 import Welcome from './Components/Welcome'
 import Message from './Components/Message'
+import Counter from './Components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Message/>
+      {/* <Message/> */}
+      <Counter/>
       {/* <Greet/>
       <Welcome/> */}
     </div>
