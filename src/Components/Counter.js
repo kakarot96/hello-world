9f32a1bd375d4b7b57bf389cb552a974224bdb51
Kunal Aggarwal <kunalaggarwal58@gmail.com>
2019-09-12 15:33:27 +0530

    Using prevState in setState

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -11,18 +11,24 @@ import React, { Component } from 'react'
     }
 
     increment(){
-        this.setState({
-            count : this.state.count + 1
-            
-        },()=>{console.log('Callback count: ',this.state.count)})
+        this.setState((prevState)=>({
+                count : prevState.count + 1
+            }
+        ),()=>{console.log('Callback count: ',this.state.count)})
         console.log(this.state.count)
     }
-        
+    incrementFive(){
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+        this.increment();
+    }    
     render() {
         return (
             <div>
                 <h1>{this.state.count}</h1>
-                <button onClick={()=> this.increment()}>Increment</button>
+                <button onClick={()=> this.incrementFive()}>Increment</button>
             </div>
         )
     }
